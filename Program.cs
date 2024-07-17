f783f48b85c068a40e033dc5c0a197870f9e5faf
akapp9709 <kappandrew2@gmail.com>
2024-07-17 20:55:54 +0200

    Day 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFile = FindFiles("stores");
+
+foreach (var file in salesFile)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folder)
+{
+    List<string> salesFiles = new List<string>();
+    var foundFiles = Directory.EnumerateFiles(folder, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
