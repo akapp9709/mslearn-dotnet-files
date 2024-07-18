8d6443ca5139d04a1ff4da7bd2733f555d749b60
akapp9709 <kappandrew2@gmail.com>
2024-07-18 11:50:08 +0200

    Day 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-var salesFile = FindFiles("stores");
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-foreach (var file in salesFile)
-{
-    Console.WriteLine(file);
-}
+var salesTotDir = Path.Combine(currentDirectory, "SalesTotalDirectory");
+Directory.CreateDirectory(salesTotDir);
+
+var salesFile = FindFiles(storesDirectory);
+var salesTotal = CalculateSalesTotal(salesFile);
+
+File.AppendAllText(Path.Combine(salesTotDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folder)
 {
@@ -15,7 +20,9 @@ IEnumerable<string> FindFiles(string folder)
 
     foreach (var file in foundFiles)
     {
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
@@ -23,3 +30,21 @@ IEnumerable<string> FindFiles(string folder)
 
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesfiles)
+{
+    double salesTotal = 0;
+
+    foreach (var file in salesfiles)
+    {
+        string salesJson = File.ReadAllText(file);
+
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        salesTotal += data?.total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData (double total);
